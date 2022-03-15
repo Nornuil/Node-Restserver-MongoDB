9987559dacdcdbb49d3f15e97dd11fdd518a4eef
unknown <wallace223@hotmail.com>
2022-03-15 17:54:01 -0300

    mongoDB post

--- models/server.js
+++ models/server.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 
+const { dbConnection } = require("../database/config");
+
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
     this.usuariosPath = "/api/usuarios";
+
+    //Conectar la base de datos para
+    this.conexDB();
     //Middlewares
     this.middlewares();
 
     //Rutas de mi aplicacion
     this.routes();
+  }
+
+  async conexDB() {
+    await dbConnection();
   }
 
   middlewares() {
